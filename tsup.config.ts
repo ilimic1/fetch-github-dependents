d6094bdb98610371e39e8512a44d6deae5caa60f
Ivo Limić <ilimic1@gmail.com>
2023-12-29 13:40:24 +0100

    enabling the package to be used as a library.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "tsup";
 
 export default defineConfig((options) => {
   return {
-    entry: ["src/cli.ts"],
+    entry: ["src/cli.ts", "src/index.ts"],
     minify: !options.watch,
-    splitting: false,
+    splitting: true,
     sourcemap: true,
     clean: true,
     dts: true,
     target: "node20",
     format: ["esm"],
+    bundle: true,
   };
 });
